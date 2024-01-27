a60546f7adf5517bcac312964c7392bbc108927a
reshma-raju-dabhade09 <157883045+reshma-raju-dabhade09@users.noreply.github.com>
2024-01-27 16:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+///////////////// Text animation section started //////////////////////////////////////////////
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.05em solid #000}";
+    document.body.appendChild(css);
+};
+
+/////////////////// Text animation section end //////////////////////////////////////////////
+/////////////////// onscroll navigation section started /////////////////////////////////////
+
+
+let sec = document.querySelectorAll('section');
+let links = document.querySelectorAll('nav ul li a');
+
+window.onscroll = () => {
+    sec.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            links.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+};
+
+///////////////////// onscroll navigation section end ////////////////////////////////
+//////////////////// onscrolltop function started here ///////////////////////////////
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        //mybutton.style.display = "block";
+        document.getElementById("nav").style.background = "rgba(255, 255, 255, 0.8)";
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("nav").style.background = "transparent";
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+/////////////////////////// onscrolltop function end here /////////////////////////////////////
